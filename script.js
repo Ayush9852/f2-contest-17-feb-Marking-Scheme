af604ee27bdc5cb7dcb2280b98a1d95dca3c6085
Ayush9852 <71625438+Ayush9852@users.noreply.github.com>
2024-02-17 12:38:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,39 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  let studentsOver50 = arr.filter(student => student.marks > 50);
+  studentsOver50.map(student => console.log(student));
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(student => {
+    if (student.marks > 50) {
+      console.log(student);
+    }
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  let newStudent = { id: 4, name: "susan", age: "20", marks: 45 };
+  arr.push(newStudent);
+  console.log(newStudent);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  arr = arr.filter(student => student.marks >= 50);
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let additionalStudents = [
+    { id: 4, name: "susan", age: "20", marks: 45 },
+    { id: 5, name: "emma", age: "21", marks: 90 },
+    { id: 6, name: "mike", age: "22", marks: 75 }
+  ];
+  arr = arr.concat(additionalStudents);
+  console.log(arr);
 }
